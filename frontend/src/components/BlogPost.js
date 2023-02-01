c8269f10b0b103e67b23265b1bcba705d34fe5af
foshesss <foshessss@gmail.com>
2023-01-31 20:01:32 -0500

    fixed naming conventions

--- frontend/src/components/BlogPost.js
+++ frontend/src/components/BlogPost.js
@@ -1,9 +1,9 @@
 const BlogPost = ({title, date, author, content}) => (
     <div class='blog-post'>
-        <h1 class='blog-post__title'>{title}</h1>
-        <h2 class='blog-post__author'>{author}</h2>
-        <h2 class='blog-post__date'>{date}</h2>
-        <p class='blog-post__content'>{content}</p>
+        <h1 className='blog-title'>{title}</h1>
+        <h2 className='blog-author'>{author}</h2>
+        <h2 className='blog-date'>{date}</h2>
+        <p className='blog-content'>{content}</p>
     </div>
 );
 
