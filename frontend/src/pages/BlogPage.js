59a635e2d13681cc906cec7ab80c15c8d368715b
golden2402 <golden2402@vt.edu>
2023-02-02 22:11:35 -0500

    add example useParams usage

--- frontend/src/pages/BlogPage.js
+++ frontend/src/pages/BlogPage.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const BlogPage = () => {
+  const { id } = useParams();
+
   return (
     <div className="blog-page">
-      
+      <p>{`You're on blog ${id}!`}</p>
     </div>
   );
 }
