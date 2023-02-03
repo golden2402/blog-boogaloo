11e355a0e1163c98279e0e170cc8b5315062b7c7
golden2402 <golden2402@vt.edu>
2023-02-03 02:13:32 -0500

    formatting & remove p element

--- frontend/src/components/BlogPost.js
+++ frontend/src/components/BlogPost.js
@@ -3,22 +3,23 @@ import { NavLink } from "react-router-dom";
 const { protocol, hostname } = window.location;
 const host = `${protocol}//${hostname}`;
 
-const BlogPost = ({title, date, author, content, id}) => {
-    const linkTo = `/blogs/${id}`;
- 
-    return (
-        <div className='blog-post'>
-            <NavLink to={linkTo}>
-                <h1 className='blog-title'>{title}</h1>
-            </NavLink>
-            <h2 className='blog-author'>{author}</h2>
-            <h2 className='blog-date'>{date}</h2>
-            <p className='blog-content'>{content}</p>
-            <NavLink to={linkTo}>
-                <p>{`Read the full post at ${host}${linkTo}`}</p>
-            </NavLink>
-        </div>
-    );
+const BlogPost = ({ title, date, author, content, id }) => {
+  const linkTo = `/blogs/${id}`;
+
+  return (
+    <div className='blog-post'>
+      <NavLink to={linkTo}>
+        <h1 className='blog-title'>{title}</h1>
+      </NavLink>
+      <h2 className='blog-author'>{author}</h2>
+      <h2 className='blog-date'>{date}</h2>
+      <p className='blog-content'>{content}</p>
+
+      <NavLink to={linkTo}>
+        {`Read the full post at ${host}${linkTo}`}
+      </NavLink>
+    </div>
+  );
 };
 
 export default BlogPost;
